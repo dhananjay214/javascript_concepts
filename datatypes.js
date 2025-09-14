61be7b45ca3c797a04c868b86a3d4cf3ee035def
dhananjay214 <dhananjaygawali214@gmail.com>
2025-09-14 11:44:34 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -36,3 +36,5 @@ console.log("typeof isActive:", typeof isActive);
 console.log("typeof notDefined:", typeof notDefined);
 console.log("typeof emptyValue:", typeof emptyValue);
 console.log("typeof sym:", typeof sym);
+
+console.log("i am trying to chaneg the files directly from main branch of git....let's see what happens")
