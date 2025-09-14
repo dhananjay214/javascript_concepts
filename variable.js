7ccfef6ffb5c993364a69b46a14452f589ab0cd2
dhananjay214 <dhananjaygawali214@gmail.com>
2025-09-14 11:11:08 +0530

    more understanding of variables

--- variable.js
+++ variable.js
@@ -1,5 +1,34 @@
 let name = "OM";
 const age = 33;
-var city = "Pune";
+var city;
+city = "Pune";
+let score;
 
 console.log(name, age, city);
+console.log(score > 0);
+
+// More variable examples
+
+let country = "India"; // let can be reassigned
+country = "Bharat";
+console.log("Country:", country);
+
+const pi = 3.14; // const cannot be reassigned
+console.log("Value of pi:", pi);
+
+var language = "JavaScript"; // var can be redeclared and reassigned
+language = "TypeScript";
+console.log("Language:", language);
+
+let notAssigned; // undefined by default
+let emptyValue = null; // explicitly set to null
+console.log("notAssigned:", notAssigned);
+console.log("emptyValue:", emptyValue);
+
+// typeof examples
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of city:", typeof city);
+console.log("Type of score:", typeof score);
+console.log("Type of notAssigned:", typeof notAssigned);
+console.log("Type of emptyValue:", typeof emptyValue);
